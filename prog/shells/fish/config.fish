0570e8a13226c9c614d004c9dc7875f473249bd9
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-01-20 19:28:18 +0530

    Fixed pet conf path for snippets.toml and modified config.fish

--- prog/shells/fish/config.fish
+++ prog/shells/fish/config.fish
@@ -1,10 +1,13 @@
 set -g fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	# Check if the fastfetch exists is available
-	if type -q fastfetch
-		fastfetch
-	end
+    if type -q starship
+	starship init fish | source
+    end
+    if type -q fastfetch
+	    fastfetch
+    end
 end
 abbr mkdir 'mkdir -p'
 alias ll='ls -latr'
