abe158d6213c67ac548b8a73d6b04a4783b1bd14
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-08-11 06:55:27 +0530

    Update
    - Better kanata config

--- prog/fzf/.module.sh
+++ prog/fzf/.module.sh
@@ -1,4 +1,5 @@
-makedir "$XDG_DATA_HOME/fzf/history"
+makedir "$XDG_STATE_HOME/fzf/history"
+touch "$XDG_STATE_HOME/fzf/history/default"
 
 # From ubuntu 20.04 this is no longer necessary.
 # run-cmd sudo add-apt-repository ppa:x4121/ripgrep
