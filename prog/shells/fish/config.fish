74b72a2b6b749a24d99f0473fe8a757c7da14530
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-11-08 19:54:22 +0530

    Update - Improved gitconfig and config.fish

--- prog/shells/fish/config.fish
+++ prog/shells/fish/config.fish
@@ -1,15 +1,20 @@
+# Skip non-interactive shells
+if not status is-interactive
+    exit
+end
+
 set -g fish_greeting
 set -g fish_command_timeout 2000
 set -g fish_key_bindings fish_vi_key_bindings
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    if type -q starship
-	starship init fish | source
-    end
-    if type -q fastfetch && test -z "$NVIM"
-	fastfetch
-    end
+# Commands to run in interactive sessions can go here
+if type -q starship
+    starship init fish | source
+    # set -x STARSHIP_CACHE $XDG_CACHE_HOME/starship
+    # set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
+end
+if type -q fastfetch && test -z "$NVIM"
+    fastfetch
 end
 set -x SHELL /usr/bin/fish
 
@@ -31,9 +36,16 @@ if type -q curl
     alias wttr='curl wttr.in'
 end
 
-bind -M insert  ctrl-e 'nvim'
-bind -M default ctrl-e 'nvim'
+if type -q fzf && type -q fd
+    bind -M insert ctrl-o 'cd (fd -t d --absolute-path | fzf)' repaint
+    bind -M default ctrl-o 'cd (fd -t d --absolute-path | fzf)' repaint
+end
 
-bind -M insert  alt-e 'sudo -E nvim' repaint
-bind -M default alt-e 'sudo -E nvim' repaint
+if type -q nvim
+    bind -M insert  ctrl-e 'nvim'
+    bind -M insert  alt-e 'sudo -E nvim' repaint
+
+    bind -M default ctrl-e 'nvim'
+    bind -M default alt-e 'sudo -E nvim' repaint
+end
 
